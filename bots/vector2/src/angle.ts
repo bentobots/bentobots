24b72203f8307cd2779be4df15400254da2e5981
John Rees <john@bitsushi.com>
2019-12-03 13:07:25 +0000

    degs and rads

--- bots/vector2/src/angle.ts
+++ bots/vector2/src/angle.ts
@@ -1,4 +1,6 @@
 import { Point, _getXY } from ".";
+import rad2deg from "./rad2deg";
+import compose from "./_compose";
 
 /**
  * Calculates the angle (in radians) of a line drawn between two points
@@ -9,5 +11,9 @@ const angle = (start: Point, end: Point): number => {
   const [x, y] = _getXY(start, end);
   return Math.atan2(y, x);
 };
+
+export const rads = angle;
+
+export const degs = compose(rad2deg, angle);
 
 export default angle;
